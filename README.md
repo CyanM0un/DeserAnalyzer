4eb321a9037858f0355f664aec0af7b5c2e2d827
CyanM0un <1544130024@qq.com>
2025-10-13 16:33:37 +0800

    init project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# DeserAnalyzer
+## DeserAnalyzer
+魔改https://github.com/kaushikjadhav01/COVID-19-Detection-Flask-App-based-on-Chest-X-rays-and-CT-Scans进行开发
---
+++ app.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 17 15:40:29 2018
+
+@author: Kaushik
+"""
+
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+import os
+import cv2
+import numpy as np
+
+UPLOAD_FOLDER = './flask_app/assets/images'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__,static_url_path='/assets',
+            static_folder='./flask_app/assets', 
+            template_folder='./flask_app')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def root():
+   return render_template('index.html')
+
+@app.route('/index.html')
+def index():
+   return render_template('index.html')
+
+@app.route('/contact.html')
+def contact():
+   return render_template('contact.html')
+
+@app.route('/news.html')
+def news():
+   return render_template('news.html')
+
+@app.route('/about.html')
+def about():
+   return render_template('about.html')
+
+@app.route('/faqs.html')
+def faqs():
+   return render_template('faqs.html')
+
+@app.route('/prevention.html')
+def prevention():
+   return render_template('prevention.html')
+
+@app.route('/upload.html')
+def upload():
+   return render_template('upload.html')
+
+@app.route('/upload_chest.html')
+def upload_chest():
+   return render_template('upload_chest.html')
+
+
+@app.route('/upload_ct.html')
+def upload_ct():
+   return render_template('upload_ct.html')
+
+@app.route('/uploaded_chest', methods = ['POST', 'GET'])
+def uploaded_chest():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            # filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload_chest.jpg'))
+    pass
+  
+@app.route('/uploaded_ct', methods = ['POST', 'GET'])
+def uploaded_ct():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            # filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload_ct.jpg'))
+    pass
+
+if __name__ == '__main__':
+   app.run()
