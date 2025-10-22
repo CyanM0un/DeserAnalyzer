f1205637146962b09aa9fb64a8d7a92af9eeeafb
czTangt <cz.tangt@gmail.com>
2025-10-22 17:44:25 +0800

    添加Docker部署

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from werkzeug.utils import secure_filename
 from utils import *
